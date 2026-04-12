d04c5676ceb102a13b8f69bb47baba5d2090142e
Kenio Natan <kenionatan@gmail.com>
2026-04-12 09:25:11 -0300

    google tag

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -32,16 +32,6 @@
     <link rel="stylesheet" href="/css/app.css">
     <style>:root{--accent-dynamic:<?= esc($buttonColor) ?>;}</style>
     
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16541205225"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'AW-16541205225');
-    </script>
-
 </head>
 <body>
     <script>document.documentElement.setAttribute('data-theme', '<?= esc($theme) ?>');</script>
